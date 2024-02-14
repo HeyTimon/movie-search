03e8f1aed3f7b0b016b118a4a84dd967c6185cad
HeyTimon <yohanestam@yahoo.com>
2024-02-15 02:06:52 +0700

    modify

--- js/script.js
+++ js/script.js
@@ -1,89 +1,172 @@
+// $('#search-button').on("click", function () {
+//   showMovie();
+// });
+
+// $('#search-input').on("keyup", function (e) {
+//     if(e.keyCode === 13){
+//         showMovie();
+//     };
+//   });
+
+// function showMovie(){
+//     $('#movie-list').html('');
+    
+//     $.ajax({
+//     url : 'http://www.omdbapi.com',
+//     type : 'get',
+//     dataType : 'json',
+//     data : {
+//       'apikey' : '7bb18cff',
+//       's' : $('#search-input').val(),
+//     },
+//     success: function (result) {
+//       if(result.Response == "True"){
+        
+//         let movies = result.Search;
+        
+//         $.each(movies,function(i, data){
+//             $('#movie-list').append(`
+//             <div class="col-md-4">
+//             <div class="card mb-3">
+//                 <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">`+ data.Title +`</h5>
+//                     <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
+//                     <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`">See Detail</a> 
+//             </div>
+//           </div>
+//         </div>
+//             `)
+//         });
+    
+//         $('#search-input').val('');
+    
+//       }else{
+//         $('#movie-list').html(`
+//         <div class="col">
+//         <h1 class="text-center">`+ result.Error +`</h1>
+//         </div>
+//         `)
+//       }
+//     }
+//     });
+    
+// };
+
+// // event  bubbling fix
+// $('#movie-list').on('click','.see-detail', function(result){
+//     $.ajax({
+//         url : 'http://www.omdbapi.com',
+//         type : 'get',
+//         dataType : 'json',
+//         data : {
+//             'apikey' : '7bb18cff',
+//             'i' : $(this).data('id'),
+//     },
+//     success: function(movie){
+//         if(movie.Response === "True"){
+//             $('.modal-body').html(`
+//                 <div class="container-fluid">
+//                     <div class="row">
+//                     <div class="col-md-4">
+//                         <img src="`+ movie.Poster +`" class="img-fluid">
+//                     </div>
+//                     <div class="col-md-8">
+//                     <ul class="list-group">
+//                     <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+//                     <li class="list-group-item">Released : `+ movie.Released +`</li>
+//                     <li class="list-group-item">Genre : `+movie.Genre+`</li>
+//                     <li class="list-group-item">Director : `+movie.Director+`</li>
+//                     <li class="list-group-item">Actors : `+ movie.Actors+`</li>
+//                   </ul>
+//                     </div>
+//                     </div>
+//                 </div>
+//             `);
+//         }
+//     }
+//     })
+// })
+
 $('#search-button').on("click", function () {
   showMovie();
 });
 
 $('#search-input').on("keyup", function (e) {
-    if(e.keyCode === 13){
-        showMovie();
-    };
-  });
+  if(e.keyCode === 13){
+    showMovie();
+  };
+});
 
 function showMovie(){
-    $('#movie-list').html('');
-    
-    $.ajax({
-    url : 'http://www.omdbapi.com',
-    type : 'get',
-    dataType : 'json',
-    data : {
-      'apikey' : '7bb18cff',
-      's' : $('#search-input').val(),
-    },
+  $('#movie-list').html('');
+  
+  $.ajax({
+    url: "https://www.omdbapi.com/?apikey=7bb18cff&s=" + $('#search-input').val(),
     success: function (result) {
       if(result.Response == "True"){
         
         let movies = result.Search;
         
         $.each(movies,function(i, data){
-            $('#movie-list').append(`
+          $('#movie-list').append(`
             <div class="col-md-4">
-            <div class="card mb-3">
+              <div class="card mb-3">
                 <img src="`+ data.Poster +`" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">`+ data.Title +`</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
-                    <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`">See Detail</a> 
+                  <h5 class="card-title">`+ data.Title +`</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
+                  <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`">See Detail</a> 
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-            `)
+          `)
         });
-    
+        
         $('#search-input').val('');
-    
+        
       }else{
         $('#movie-list').html(`
-        <div class="col">
-        <h1 class="text-center">`+ result.Error +`</h1>
-        </div>
+          <div class="col">
+            <h1 class="text-center">`+ result.Error +`</h1>
+          </div>
         `)
       }
     }
-    });
-    
-};
-
-// event  bubbling fix
-$('#movie-list').on('click','.see-detail', function(result){
+  });
+  
+  // event  bubbling fix
+  $('#movie-list').on('click','.see-detail', function(result){
     $.ajax({
-        url : 'http://www.omdbapi.com',
-        type : 'get',
-        dataType : 'json',
-        data : {
-            'apikey' : '7bb18cff',
-            'i' : $(this).data('id'),
-    },
-    success: function(movie){
+      url: "https://www.omdbapi.com",
+      type: 'get',
+      dataType: 'json',
+      data: {
+        'apikey': '7bb18cff',
+        'i': $(this).data('id'),
+      },
+      success: function(movie){
         if(movie.Response === "True"){
-            $('.modal-body').html(`
-                <div class="container-fluid">
-                    <div class="row">
-                    <div class="col-md-4">
-                        <img src="`+ movie.Poster +`" class="img-fluid">
-                    </div>
-                    <div class="col-md-8">
-                    <ul class="list-group">
+          $('.modal-body').html(`
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="`+ movie.Poster +`" class="img-fluid">
+                </div>
+                <div class="col-md-8">
+                  <ul class="list-group">
                     <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
                     <li class="list-group-item">Released : `+ movie.Released +`</li>
                     <li class="list-group-item">Genre : `+movie.Genre+`</li>
                     <li class="list-group-item">Director : `+movie.Director+`</li>
                     <li class="list-group-item">Actors : `+ movie.Actors+`</li>
                   </ul>
-                    </div>
-                    </div>
                 </div>
-            `);
+              </div>
+            </div>
+          `);
         }
-    }
+      }
     })
-})
+  });
+}
